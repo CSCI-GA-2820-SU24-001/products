ba35fa071e15cd7a8b4964ea1e88db5b73ab4571
disintegrate-Fred <xz2596@nyu.edu>
2024-06-26 19:48:31 +0000

    Updated Product model and corresponding tests

--- tests/test_models.py
+++ tests/test_models.py
@@ -6,8 +6,8 @@ import os
 import logging
 from unittest import TestCase
 from wsgi import app
-from service.models import YourResourceModel, DataValidationError, db
-from .factories import YourResourceModelFactory
+from service.models import Product, DataValidationError, db
+from .factories import ProductFactory
 
 DATABASE_URI = os.getenv(
     "DATABASE_URI", "postgresql+psycopg://postgres:postgres@localhost:5432/testdb"
@@ -15,11 +15,11 @@ DATABASE_URI = os.getenv(
 
 
 ######################################################################
-#  YourResourceModel   M O D E L   T E S T   C A S E S
+#  Product   M O D E L   T E S T   C A S E S
 ######################################################################
 # pylint: disable=too-many-public-methods
-class TestYourResourceModel(TestCase):
-    """Test Cases for YourResourceModel Model"""
+class TestProduct(TestCase):
+    """Test Cases for Product Model"""
 
     @classmethod
     def setUpClass(cls):
@@ -37,7 +37,7 @@ class TestYourResourceModel(TestCase):
 
     def setUp(self):
         """This runs before each test"""
-        db.session.query(YourResourceModel).delete()  # clean up the last tests
+        db.session.query(Product).delete()  # clean up the last tests
         db.session.commit()
 
     def tearDown(self):
@@ -48,15 +48,74 @@ class TestYourResourceModel(TestCase):
     #  T E S T   C A S E S
     ######################################################################
 
-    def test_example_replace_this(self):
-        """It should create a YourResourceModel"""
-        # Todo: Remove this test case example
-        resource = YourResourceModelFactory()
+    def test_create_a_resource(self):
+        """It should create a Product"""
+        resource = ProductFactory()
         resource.create()
         self.assertIsNotNone(resource.id)
-        found = YourResourceModel.all()
+        found = Product.all()
         self.assertEqual(len(found), 1)
-        data = YourResourceModel.find(resource.id)
+        data = Product.find(resource.id)
         self.assertEqual(data.name, resource.name)
 
-    # Todo: Add your test cases here...
+    def test_create_a_resource_with_missing_name(self):
+        """It should not create a Product with missing name"""
+        resource = ProductFactory()
+        resource.name = None
+        with self.assertRaises(DataValidationError):
+            resource.create()
+
+    def test_read_a_resource(self):
+        """It should read a Product by ID"""
+        resource = ProductFactory()
+        resource.create()
+        data = Product.find(resource.id)
+        self.assertIsNotNone(data)
+        self.assertEqual(data.id, resource.id)
+        self.assertEqual(data.name, resource.name)
+
+    def test_read_a_non_existent_resource(self):
+        """It should not read a non-existent Product"""
+        data = Product.find(0)
+        self.assertIsNone(data)
+
+    def test_update_a_resource(self):
+        """It should update a Product"""
+        resource = ProductFactory()
+        resource.create()
+        original_id = resource.id
+        resource.name = "Updated Name"
+        resource.update()
+        updated = Product.find(original_id)
+        self.assertEqual(updated.name, "Updated Name")
+
+    def test_update_a_resource_with_invalid_data(self):
+        """It should not update a Product with invalid data"""
+        resource = ProductFactory()
+        resource.create()
+        resource.name = None
+        with self.assertRaises(DataValidationError):
+            resource.update()
+
+    def test_delete_a_resource(self):
+        """It should delete a Product"""
+        resource = ProductFactory()
+        resource.create()
+        resource.delete()
+        data = Product.find(resource.id)
+        self.assertIsNone(data)
+
+    def test_delete_a_non_existent_resource(self):
+        """It should not delete a non-existent Product"""
+        resource = ProductFactory()
+        resource.id = 0
+        with self.assertRaises(DataValidationError):
+            resource.delete()
+
+    def test_list_all_resources(self):
+        """It should list all Products"""
+        resources = [ProductFactory() for _ in range(5)]
+        for resource in resources:
+            resource.create()
+        found = Product.all()
+        self.assertEqual(len(found), 5)
